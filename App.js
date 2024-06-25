020fa8632a98de8c9b642ec0382b77236d48a2a1
aasjunior <adeldivo@hotmail.com>
2024-06-24 22:44:20 -0300

    modal

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { useState } from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native'
 import Slider from '@react-native-community/slider'
+import { ModalPassword } from './src/components/modal'
+
+let charset = '%9&X8WD9WRwybHpu6LS3tZf^H9R^#jV%^SdVK$DbesQunfzDpG44cB79UP&Do2^R7tD5hj5s@&@Lz6cY5wWjattuDmWNUe$Xv^W#'
 
 export default function App(){
+  const [size, setSize] = useState(10)
+  const [passwordValue, setPasswordValue] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function generatePassword(){
+    let password = ''
+    for(let i = 0, n = charset.length; i < size; i++){
+      password += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    setPasswordValue(password)
+    setModalVisible(true)
+  }
 
   return(
     <View style={styles.container}>
@@ -11,22 +28,28 @@ export default function App(){
         resizeMode='contain'
       />
 
-      <Text style={styles.title}>20 caracteres</Text>
+      <Text style={styles.title}>{size} caracteres</Text>
 
       <View style={styles.area}>
-        <Slider 
+        <Slider
           style={{ height: 50 }}
           minimumValue={6}
           maximumValue={20}
           maximumTrackTintColor='#FF0000'
           minimumTrackTintColor='#000'
           thumbTintColor='#392de9'
+          value={size}
+          onValueChange={ (value) => setSize(value.toFixed(0)) }
         />
       </View>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
         <Text style={styles.buttonText}>Gerar senha</Text>
       </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType='fade' transparent={true}>
+        <ModalPassword password={passwordValue} handleClose={ () => setModalVisible(false) }/>
+      </Modal>
     </View>
   )
 }
